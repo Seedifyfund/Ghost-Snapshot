c8b2a37d46cdb8e365457dafa409c3c699cdcfa8
Aziz <ansariaziz001@gmail.com>
2021-12-31 11:30:03 +0530

    add jwt expiration time 48hrs

--- helper/jwtUtils.js
+++ helper/jwtUtils.js
@@ -2,12 +2,13 @@ const jwt = require("jsonwebtoken");
 
 const jwtUtils = {};
 
-jwtUtils.getAuthToken = (data) => jwt.sign(data, process.env.SECRET);
+jwtUtils.getAuthToken = (data) => jwt.sign(data, process.env.SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
 
 jwtUtils.decodeAuthToken = (token) => {
   if (token) {
+
     try {
-      return jwt.decode(token, process.env.SECRET);
+      return jwt.verify(token, process.env.SECRET);
     } catch (error) {
       return false;
     }
