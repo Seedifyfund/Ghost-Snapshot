e5d7340586459918d71de722a206bab433e49e5f
Aziz <ansariaziz001@gmail.com>
2022-02-01 17:51:11 +0530

    list api for igo

--- modules/projects/projectsController.js
+++ modules/projects/projectsController.js
@@ -32,4 +32,38 @@ projectsCtr.addNewProject = async (req, res) => {
     });
   }
 };
+projectsCtr.list = async (req, res)=>{
+  try {
+    let query = {};
+    if (req.query.search) {
+      query.name = new RegExp(`${req.query.search}`, 'i');
+    }
+    let page = req.query.page ? req.query.page : 1;
+    let list = await ProjectModel.find(query, {subscribedUsers : 0})
+      .skip((+page - 1 || 0) * +process.env.LIMIT)
+      .limit(+process.env.LIMIT)
+      .sort({ createdAt: -1 })
+      .lean();
+
+    const totalCount = await ProjectModel.countDocuments(query);
+    const pageCount = Math.ceil(totalCount / +process.env.LIMIT);
+    return res.status(200).json({
+      message: "SUCCESS",
+      status: true,
+      data: list,
+      pagination: {
+        pageNo: page,
+        totalRecords: totalCount,
+        totalPages: pageCount,
+        limit: +process.env.LIMIT,
+      },
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: "DB_ERROR",
+      status: true,
+      err: err.message ? err.message : err,
+    });
+  }
+}
 module.exports = projectsCtr;
