eb20ec423e66d9a67ae9ce7253dd6e7830882c5c
SurajDev-Minddeft <suraj.chitroda@minddeft.com>
2022-07-26 17:16:29 +0530

    changed admin lock time to 12hr

--- modules/admin/adminRoute.js
+++ modules/admin/adminRoute.js
@@ -2,10 +2,8 @@ const express = require('express');
 const AdminCtr = require('./adminController');
 const AdminMiddleware = require('./adminMiddleware');
 const Auth = require('../../helper/auth');
-const rateLimit = require('express-rate-limit');
 
 const adminRoute = express.Router();
-const reqLimiter = rateLimit({ max: 10, windowMS: 1000 * 60 * 10 }) // 10 request per 10 minutes
 
 // get roles
 const addNewAdmin = [
@@ -17,7 +15,7 @@ const addNewAdmin = [
 adminRoute.post('/add', addNewAdmin);
 
 // login admin
-const login = [reqLimiter ,AdminMiddleware.validateLogin, AdminCtr.login];
+const login = [AdminMiddleware.validateLogin, AdminCtr.login];
 adminRoute.post('/login', login);
 
 module.exports = adminRoute;
